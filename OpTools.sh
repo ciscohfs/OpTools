74ba8756c3b4c8232b1c7b08e43cd68d33432a65
ciscohfs <acb94981@gmail.com>
2024-06-04 14:45:04 +0330

    create if lvl1

--- OpTools.sh
+++ OpTools.sh
@@ -1,1 +1,35 @@
-dkkkdkkdkk
+#! /bin/bash
+
+
+# start text and input for your op
+echo "hello my dear
+thanks for clone this tool"
+echo "."
+sleep 0.5
+echo "."
+sleep 0.5
+echo "."
+sleep 0.5
+echo "."
+sleep 0.5
+echo "."
+sleep 0.5
+echo "."
+read -p "you have linux(debian distros) or windows ? W/L " yourOP
+
+#create if for tool
+if [ $yourOP == "W" ]; then
+    echo "W"
+    bash OpTools.sh
+elif [ $yourOP == "w" ]; then
+    echo "w"
+    bash OpTools.sh
+elif [ $yourOP == "L" ]; then
+    echo "L"
+    bash OpTools.sh
+elif [ $yourOP == "l" ]; then
+    echo "l"
+    bash OpTools.sh
+fi
+
+
